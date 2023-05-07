2e51c1776f648df2eb654db348c78cd7c66d43d9
janellewong <janellewong112@gmail.com>
2023-05-06 23:34:06 -0700

    add work experience

---
+++ src/components/experience.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+import headshot from "../img/headshot.jpg";
+
+class Experience extends React.Component {
+  render() {
+    return (
+      <section id="experience" className="about-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Work Experience</h3>
+                <p className="subtitle-a">
+                  software engineer & designer
+                </p>
+                <div className="line-mf"></div>
+              </div>
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-3">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-6">
+                        <div className="col-md-12">
+                          <h6 className="title-left">Salesforce</h6>
+                          <br></br>
+                          <p>MAY 2023 - PRESENT</p>
+                          <p>Software Engineer Intern</p>
+                          <p>San Francisco, California</p>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-9">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <div className="col-md-12">
+                          <p>In progress!</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-3">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-6">
+                        <div className="col-md-12">
+                          <h6 className="title-left">Salesforce</h6>
+                          <br></br>
+                          <p>JAN 2022 - AUG 2022</p>
+                          <p>Software Engineer Intern</p>
+                          <p>Vancouver, British Columbia</p>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-9">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <div className="col-md-12">
+                        <ul>
+                          <li>Designed and implemented a full stack project, creating multiple filter panel components by adding new endpoints to various REST APIs in Java to query and filter the database tables </li>
+                          <li>Tested REST APIs by writing functional tests and documented such changes of these public APIs</li>
+                          <li>Implemented the panel frontend with Salesforce Lightning Web Components framework in Typescript and modified the Salesforce wire adapter to connect the front and back ends</li>
+                          <li>Tested the panel’s proper end-to-end integration with jest mock and snapshot tests</li>
+                        </ul>
+                        <p><b>Skills: </b>Typescript, Salesforce Lightning Web Components, Java, Perforce, Git, GUS, Jest, API Testing, REST API, Agile</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-3">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-6">
+                        <div className="col-md-12">
+                          <h6 className="title-left">UBC BizTech</h6>
+                          <br></br>
+                          <p>APR 2021 - MAY 2022</p>
+                          <p>Design Lead</p>
+                          <p>Vancouver, British Columbia</p>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-9">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <div className="col-md-12">
+                        <br></br>
+                        <ul>
+                          <li>Managed a team of graphic designers, web designers, and photographers to create logos and graphics to promote and represent the club and our unique events</li>
+                          <li>Conceptualized and created graphics for BizTech’s events and social media promotions using Figma and Adobe Photoshop</li>
+                        </ul>
+                        <p><b>Skills: </b>Adobe Photoshop, Figma, Procreate, Webflow</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-3">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-6">
+                        <div className="col-md-12">
+                          <h6 className="title-left">Major League Hacking</h6>
+                          <br></br>
+                          <p>JUN 2021 - AUG 2021</p>
+                          <p>Production Engineering</p>
+                          <p>Remote</p>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-9">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <div className="col-md-12">
+                        <br></br>
+                        <ul>
+                          <li>Built a scalable, containerized Flask website with a postgreSQL database deployed with an AWS server</li>
+                          <li>Set up a CI/CD pipeline through GitHub Actions and monitored the website with cAdvisor by running a bash script automatically upon deployment</li>
+                          <li>Administered, configured, and upgraded Linux systems and infrastructure</li>
+                        </ul>
+
+                        <p><b>Skills: </b>Python, Flask, AWS, Bootstrap, Nginx, Gunicorn, Git, Certbot, PostgreSQL, Docker, Kubernetes, cAdvisor, Prometheus, Grafana, FirewallD, YAML, Github Actions</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              
+            </div>
+          </div>
+          
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Experience;
--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import * as serviceWorker from './serviceWorker';
 import Navbar from './components/navbar.jsx';
 import Intro from './components/intro.jsx';
 import About from './components/about.jsx';
+import Experience from './components/experience';
 import Portfolio from './components/portfolio.jsx';
 import Contact from './components/contact.jsx';
 import BackToTop from './components/back-top.jsx';
@@ -35,6 +36,7 @@ ReactDOM.render(
         <Navbar />
         <Intro />
         <About />
+        <Experience />
         <Portfolio />
         <Contact />
         <BackToTop />
